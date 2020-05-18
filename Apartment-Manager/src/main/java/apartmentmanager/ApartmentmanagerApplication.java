b6d2c4e072df2008f445fc3bfd8e74e917e8d9f8
tungnd1105 <64002250+tungnd1105@users.noreply.github.com>
2020-05-18 11:48:44 +0700

    new

---
+++ Apartment-Manager/src/main/java/apartmentmanager/ApartmentmanagerApplication.java
@@ -0,0 +1,13 @@
+package apartmentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApartmentmanagerApplication.class, args);
+    }
+
+}
---
+++ Apartment-Manager/src/main/java/apartmentmanager/controller/ViewController.java
@@ -0,0 +1,17 @@
+package apartmentmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    @GetMapping("/")
+    public String pageadmin() {
+        return "layout/LayoutAdmin";
+    }
+    @GetMapping("/residential")
+    public String pageresidential() {
+        return "components/Residential";
+    }
+
+}
