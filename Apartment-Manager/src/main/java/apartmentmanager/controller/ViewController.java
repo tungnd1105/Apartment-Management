4c3900a19e7cc451b8d17aa476e86f5ace947be3
tungnd1105 <64002250+tungnd1105@users.noreply.github.com>
2020-05-19 18:57:17 +0700

    update

--- Apartment-Manager/src/main/java/apartmentmanager/controller/ViewController.java
+++ Apartment-Manager/src/main/java/apartmentmanager/controller/ViewController.java
@@ -5,16 +5,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
+    // return template page admin
     @GetMapping("/")
     public String pageadmin() {
         return "layout/LayoutAdmin";
     }
+
+    // return template page residential
     @GetMapping("/residential")
     public String pageresidential() {
         return "components/Residential";
     }
+
+    // return template page table residential
     @GetMapping("/listResidential")
-    public String pagelistresidential() {
-        return "components/ListResidential";
+    public String pagebuildingresidential() {
+        return "components/TableResidential";
+    }
+
+    // return template page table building
+    @GetMapping("/tablebuilding")
+    public String pagetablebuilding() {
+        return "components/TableBuilding";
     }
 }
