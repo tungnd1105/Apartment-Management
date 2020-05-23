79dc69ee193c18007b83c6a5831ed419fee9866c
tungnd1105 <64002250+tungnd1105@users.noreply.github.com>
2020-05-23 17:46:43 +0700

    delete

--- Apartment-Manager/src/main/java/apartmentmanager/controller/ViewController.java
+++ Apartment-Manager/src/main/java/apartmentmanager/controller/ViewController.java
@@ -12,40 +12,15 @@ public class ViewController {
     }
 
     // return template page residential
-    @GetMapping("/residential")
-    public String pageresidential() {
-        return "components/quanly/Residential";
+    @GetMapping("/cudan")
+    public String pageCudan() {
+        return "components/quanly/Cudan";
     }
 
     // return template page table residential
-    @GetMapping("/listResidential")
-    public String pagebuildingresidential() {
-        return "components/quanly/TableResidential";
+    @GetMapping("/tablecudan")
+    public String pageTableCudan() {
+        return "components/quanly/TableCudan";
     }
 
-    // return template page table building
-    @GetMapping("/tablebuilding")
-    public String pagetablebuilding() {
-        return "components/quanly/TableBuilding";
-    }
-    // return template page table building
-    @GetMapping("/tableroom")
-    public String pagetableroom() {
-        return "components/quanly/TableRoom";
-    }
-    // return template page table Invoice
-    @GetMapping("/tableinvoice")
-    public String pagetableinvoice() {
-        return "components/quanly/TableInvoice";
-    }
-    // return template page table Invoice detail
-    @GetMapping("/tableinvoicedetail")
-    public String pagetableinvoicedetail() {
-        return "components/quanly/TableInvoiceDetial";
-    }
-    // return template page table Invoice detail
-    @GetMapping("/tablevehicle")
-    public String pagetablevehicle() {
-        return "components/quanly/TableVehicle";
-    }
 }
